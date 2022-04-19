7138405fa1828fc0eeca6bef7f741aaa33a308fb
BFHENG <baptiste.heng.s@gmail.com>
2022-04-19 17:34:11 +0200

    2 modèles

--- topoguidebaptisteheng/itineraires/views.py
+++ topoguidebaptisteheng/itineraires/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
+
+def liste_itineraires(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
 # Create your views here.
