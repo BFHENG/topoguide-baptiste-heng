02dc8619de3fa6c84bb4b0a735b83218189fcd61
BFHENG <baptiste.heng.s@gmail.com>
2022-04-21 08:57:32 +0200

    formulaire création de sujet fonctionnel

--- topoguidebaptisteheng/itineraires/urls.py
+++ topoguidebaptisteheng/itineraires/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     
     path('<int:itineraire_id>/<int:sortie_id>/', views.details_sortie, name = 'details_sortie'),
     
-    path('<int:itineraire_id>/nouvelle_sortie/', views.nouvelle_sortie, name = 'nouvelle_sortie'),
+    path('nouvelle_sortie/', views.nouvelle_sortie, name = 'nouvelle_sortie'),
     
     #path('<int:itineraire_id>/', views.modif_sortie, name = 'modif_sortie')
 ]
--- topoguidebaptisteheng/itineraires/views.py
+++ topoguidebaptisteheng/itineraires/views.py
@@ -4,15 +4,12 @@ from .models import Itineraire, Sortie
 from django.http import HttpResponse
 from django.template import loader
 
+from .forms import SortieForm
+
 def liste_itineraires(request):
     
     liste_itineraires = get_list_or_404(Itineraire)
-    #template = loader.get_template('itineraires/liste_itineraires.html')
-    #context = {
-    #    'liste_itineraires': liste_itineraires,
-    #}
     
-    #return HttpResponse(template.render(context, request))
     return render(request, 'itineraires/liste_itineraires.html', {'liste_itineraires': liste_itineraires})
 
 def details_itineraire(request, itineraire_id):
